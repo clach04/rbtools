d08ae412bec1a3a603e158a4841a6b41d5571c2a
clach04 <Chris.Clark@actian.com>
2012-09-12 15:12:37 -0700

    Prelimimary Windows exe generation and source packaging.
    Added piccolo specific setup script for source code and for win32
    exe.

---
+++ p2_readme.txt
@@ -0,0 +1,65 @@
+readme for Ingres/Picolo version of ReviewBoard (rbtools) postreview
+
+NOTE this postreview supports SVN (code.ingres.com) and Piccolo,
+support for other SCM's has been removed.
+
+
+NOTE recommended minimum version of piccolo client is 2.2.1alpha
+when used with ReviewBoard (for binary and deleted file improvements
+on diffs/rcompare).
+
+However Windows users should really use version 2.2.4 for the
+MASSIVE performance improvements is strongly recommended.
+
+Under Windows, can either have
+
+* really small Python script (~200Kb) + Python installed
+* smaller (compared with above) sized exe.
+
+To use (assuming using the exe), place exe (and supllement dlls/exes)
+in path. If not using the exe replace with calls to python.
+
+
+Assuming user called FRED:
+
+    postreview --server=http://reviewboard.ingres.prv --submit-as=FRED
+
+NOTE piccolo (p[.exe]) must be in the path.
+
+This will post all changes to reviewboard and return you a review number/URL.
+
+If you need to update the review (e.g. make minor changes) simply re-post
+with the review number. E.g. assuming review number XXXX:
+
+    postreview --server=http://reviewboard.ingres.prv --submit-as=FRED -rXXXX
+
+
+Under VMS subprocess spawning is tricky so instead perform diffs manually
+to a file and then feed that file to postreview. This can be done on any
+platform but is *required* under VMS.
+
+E.g.
+
+    p working | p rcompare -l - > SMALL_EXAMPLE_PIC.DIFF
+    postreview --server=http://reviewboard.ingres.prv --submit-as=FRED --p2-diff-filename=SMALL_EXAMPLE_PIC.DIFF
+
+Can also post review titles, testing done from a file, etc.
+
+    postreview --server=http://reviewboard.ingres.prv --submit-as=FRED  --summary="IP for my cool changes" --testing-done-file=testing_demo.txt
+
+
+For more information and help issue:
+
+    postreview -h
+
+
+All Piccolo specific flags are marked with "p2"
+
+
+Release History
+---------------
+
+20090910    Initial.
+
+20091218    Auto fills in; bugs, branch, group (mailing lists).
+
---
+++ p2_setup.py
@@ -0,0 +1,75 @@
+"""
+Issue:
+
+NOTE using Python 2.4, this results in an exe about 4Mb in size.
+NOTE using Python 2.6, this results in an exe about 5.5Mb in size.
+
+    E:\Python24\python.exe ingres_setup.py py2exe
+    c:\python24\python ingres_setup.py py2exe
+    setup.py py2exe
+
+Quick-N-Dirty create win32 binaries and zip file script.
+Zero error checking.
+
+TODO inject 'py2exe' into sys.argv?
+"""
+import os
+import sys
+import glob
+import shutil
+
+from distutils.core import setup
+
+import py2exe
+
+
+# Clean temp Python/Jython files
+delete_list = glob.glob('simplejson/*.pyc') + glob.glob('simplejson/*$py.class')
+for x in delete_list:
+    os.remove(x)
+
+try:
+    shutil.rmtree('dist')
+except WindowsError, info:
+    # assume directory does not exist
+    pass
+
+print 'nasty copy hack'
+shutil.copy2(os.path.join('rbtools', 'postreview.py'), 'postreview.py')
+
+if len(sys.argv) == 1:
+    print 'defaulting to creating py2exe'
+    sys.argv += ['py2exe']
+
+# disable optimization- we _may_ need docs strings, specifically "copyright"
+setup(
+    options={"py2exe": {
+                            #"includes": ["decimal"],
+                            "optimize": 1,  # 1 and NOT 2 because I use the __doc__ string as the usage string. 2 optimises out the doc strings
+                            'bundle_files': 1,
+                            ## options to reduce size of final exe
+                            #~ 'ascii': True,  # Exclude encodings
+                            'excludes': [
+                                        '_ssl',  # Exclude _ssl
+                                        'pyreadline',  # 'difflib', 
+                                        'doctest',  # 'locale',
+                                        #'optparse', 
+                                        'pickle',  # 'calendar',# Exclude standard library
+                                        #'re',
+                                        ],  
+                            }
+            },
+    zipfile=None,  # try and make a single exe, if do not want this loose this and the 'bundle_files' option
+    console=['postreview.py']
+    )
+
+zipfilename = 'distribute_me.zip'
+zipfilelist = ['p2_readme.txt', 'postreview.py', os.path.join('win32bin', 'diff.exe'), os.path.join('win32bin', 'p.exe')] + glob.glob('rbtools/*.py') + glob.glob('rbtools/*/*.py') + glob.glob('simplejson/*') + glob.glob('dist/*')
+
+import zipfile
+z = zipfile.ZipFile(zipfilename, 'w')
+for x in zipfilelist:
+    z.write(x)
+z.close()
+
+print 'Created:', zipfilename
